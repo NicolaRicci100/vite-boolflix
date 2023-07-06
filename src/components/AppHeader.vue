11e087f007bdc179189543878a619993b044861b
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-06 16:49:52 +0200

    creo AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+  <nav class="d-flex">
+    <h1>Boolflix</h1>
+    <!-- <SearchBar /> -->
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
