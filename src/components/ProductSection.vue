7b386b4e8a1d27c9ceb3bb0608c0ad8c8a0bd7ec
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-06 19:14:43 +0200

    aggiunte bandiere

--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -2,6 +2,16 @@
 export default {
   props: {
     productType: Object
+  },
+  computed: {
+    isFlag() {
+      const flags = ['it', 'en'];
+      return flags.includes(this.productType.original_language);
+    },
+    srcFlag() {
+      const url = new URL(`../assets/img/${this.productType.original_language}.png`, import.meta.url)
+      return url.href;
+    }
   }
 }
 </script>
@@ -10,11 +20,19 @@ export default {
   <ul>
     <li>{{ productType.title || productType.name }}</li>
     <li>{{ productType.original_title || productType.original_name }}</li>
-    <li>{{ productType.original_language }}</li>
+    <li>
+      <img v-if="isFlag" :src="srcFlag" :alt="productType.original_language">
+      <span v-else>{{ productType.original_language }}</span>
+    </li>
     <li>{{ productType.vote_average }}</li>
   </ul>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/style.scss';
+
+img {
+  height: 20px;
+  width: 30px;
+}
 </style>
