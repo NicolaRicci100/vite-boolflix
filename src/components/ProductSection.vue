764adf1582b62c0c65926b55654d6420251c1aba
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-07 16:28:42 +0200

    aggiusto stile

--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -47,14 +47,16 @@ export default {
 
   <figure class="poster">
     <img :src="posterPath" :alt="productType.title || productType.name">
-    <div class="description">
+    <div class="description p-3">
       <h4>{{ productType.title || productType.name }}</h4>
       <div>{{ productType.original_title || productType.original_name }}</div>
-      <div class="flag">
+      <div class="flag py-2 d-flex align-items-center">
+        <span class="me-2">Lingua: </span>
         <img v-if="isFlag" :src="srcFlag" :alt="productType.original_language">
         <span v-else>{{ productType.original_language }}</span>
       </div>
       <div>
+        <span>Voto: </span>
         <i v-for="n in 5" :key="n" :class="starVote(n)" class="fa-star"></i>
       </div>
     </div>
@@ -65,37 +67,41 @@ export default {
 @use '../assets/scss/style.scss';
 
 .description {
-
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   opacity: 0;
-
+  width: 100%;
+  height: 100%;
 }
 
 .poster {
-  height: 400px;
+  height: 450px;
   width: 300px;
-  margin: 10px 10px;
+  margin: 10px 20px;
   position: relative;
+  cursor: pointer;
 
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
     display: block;
-    object-position: 50% 0;
     transition: .5s ease;
   }
 }
 
 .poster:hover>img {
-  opacity: 0.4;
+  opacity: 0.2;
 }
 
 .poster:hover .description {
   opacity: 1;
   transition: .5s ease;
+}
+
+.flag {
+  width: 30px;
 }
 </style>
